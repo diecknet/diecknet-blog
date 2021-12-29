187e43c44d15aec2b5b03d7c83cd1c3a85440a1e
diecknet <54552094+diecknet@users.noreply.github.com>
2021-12-29 22:37:49 +0100

    fix url for sharing & rss

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -4,7 +4,7 @@ module.exports = {
     // Website title, shown in left sidebar and in page title
     title: "diecknet",
     // Site URL to generate absolute URLs. Used across the board.
-    url: process.env.URL || "http://localhost:8080",
+    url: "https://diecknet.de",
     // Profile image for left sidebar
     image: "/assets/images/sidebar_profile.jpg",
     // Image alt text for left sidebar
